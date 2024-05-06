e8bd2b1dd046e546bcfb326dbdafa95b4199aad0
Matt Schuman <mattschuman43@gmail.com>
2024-05-06 12:41:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import string
+#Generate dictionary for storing the character counts
 historygram = {char: 0 for char in string.ascii_lowercase}
 try:
+    #open file
     with open('/var/lib/words/dict', 'r') as file:
         for line in file:
+            #remove whitespace and convert to lower
             word = line.strip().lower()
             for char in word:
+                #add character to correct dictionary item
                 if char in historygram:
                     historygram[char] += 1
     print(historygram)
